5a213f3cc9bb3ba72f0a762082a5652983bacc54
akerfan1 <124583392+akerfan1@users.noreply.github.com>
2026-08-11 16:31:00 +0330

    Update index.ts

--- supabase/functions/v2ray-worker/index.ts
+++ supabase/functions/v2ray-worker/index.ts
@@ -58,7 +58,7 @@ const serverGroups = {
           "vless://b56b8e9c-b338-4d42-87c0-35ce808f41eb@104.21.74.63:8443?encryption=none&security=tls&sni=gorbahh.erfanhub.ir&fp=random&insecure=0&allowInsecure=0&type=ws&host=gorbahh.erfanhub.ir&path=%2Fproxyip%3D23.94.103.194#7",
           "vless://a08bcfa1-f3b2-44f2-8899-d19ea09fe9d2@91.206.71.164:443?encryption=none&security=tls&sni=NuJ-VdqKRjD716Kg0CVB20.ErfnmnDI474.WoRkErs.Dev&fp=chrome&alpn=http%2F1.1&insecure=0&allowInsecure=0&type=ws&host=nuj-vdqkrjd716kg0cvb20.erfnmndi474.workers.dev&path=%2Fvl%2FHkFiKqHSWqoacZM7R0daqmO%3Fed%3D2560#8",
           "trojan://%2CZ%268JdNdGb%2BRG%3BXV-U6W@104.16.75.207:443?security=tls&sni=IOwzJ-QEnL8r7zjVMU7KlXdctT2r.WoDIwow334.wOrKeRS.dEv&fp=chrome&alpn=http%2F1.1&insecure=0&allowInsecure=0&type=ws&host=iowzj-qenl8r7zjvmu7klxdctt2r.wodiwow334.workers.dev&path=%2Ftr%2FazRMJyJ3O901Nvq6JNjsGYwJx0Jpl6%3Fed%3D2560#9",
-          "vless://d0937cc7-fac6-efe4-f6fc-4b7564fc9065@103.21.244.26:443?encryption=none&security=tls&sni=dsad4073.v6qnd9c1.workers.dev&fp=random&alpn=http%2F1.1&insecure=0&allowInsecure=0&type=ws&host=dsad4073.v6qnd9c1.workers.dev&path=%2Fline-vpn-production.up.railway.app%2Fws%2Fd0937cc7-fac6-efe4-f6fc-4b7564fc9065#10",
+          "vless://b585dc5e-55bf-4a8b-913a-27c9ccac05c3@103.21.244.26:443?encryption=none&security=tls&sni=tesat.wodiwow334.workers.dev&fp=chrome&alpn=http%2F1.1&insecure=0&allowInsecure=0&type=ws&host=tesat.wodiwow334.workers.dev&path=%2Fbab-6.site%3A443%2Fvws%2F#10",
   ],
   "3": [
        "vless://14e28b36-9a4d-40a4-9aff-fb63163c74f4@172.64.229.36:443?encryption=none&security=tls&sni=test1200.erfanhub.ir&fp=random&alpn=http%2F1.1&insecure=0&allowInsecure=0&type=ws&host=test1200.erfanhub.ir&path=%2Fpyip%3D178.156.139.174#8",
