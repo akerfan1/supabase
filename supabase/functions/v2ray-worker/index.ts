4edde87ec157e3e1d303da36569f35d0f3389f41
akerfan1 <124583392+akerfan1@users.noreply.github.com>
2026-09-14 11:37:43 +0330

    Update group 1

--- supabase/functions/v2ray-worker/index.ts
+++ supabase/functions/v2ray-worker/index.ts
@@ -46,7 +46,7 @@ Deno.serve(async (request) => {
 const serverGroups = {
  "1": [
         "vless://50414e45-4c5f-5a45-5553-3448ea55ea1a@104.25.102.179:443?encryption=none&security=tls&sni=ezzukteyue1s.akerfan180.workers.dev&fp=unsafe&type=ws&host=ezzukteyue1s.akerfan180.workers.dev&path=%2Fstream%2FPANEL_ZEUS%2F3448ea55ea1a#1",
-        "vless://50414e45-4c5f-5a45-5553-cd490c072370@45.12.30.125:443?encryption=none&security=tls&sni=gdz543ezu4ds.v6qnd9c1.workers.dev&fp=unsafe&type=ws&host=gdz543ezu4ds.v6qnd9c1.workers.dev&path=%2Fstream%2FPANEL_ZEUS%2Fcd490c072370#2",
+        "vless://50414e45-4c5f-5a45-5553-cd490c072370@45.12.30.125:443?encryption=none&security=tls&sni=gdz543ezu4ds.v6qnd9c1.workers.dev&fp=unsafe&type=ws&host=gdz543ezu4ds.v6qnd9c1.workers.dev&path=%2Fstream%2FPANEL_ZEUS%2Fcd490c072370%2Floc-1#2",
         "vless://efd26d58-6fc6-4999-a02c-0ca13879a756@188.114.97.8:443?encryption=none&security=tls&sni=KI3aGm44dqq-2dJpSbGFKU71VU.DOCom47457.woRKErs.DEV&fp=chrome&alpn=http%2F1.1&type=ws&host=ki3agm44dqq-2djpsbgfku71vu.docom47457.workers.dev&path=%2Fvl%2FbZhNE35Gca3Tl4JrL3H34BpmfMoO9KL%3Fed%3D2560#3",
         "vless://292032c7-15a3-4eaf-8d76-076c13832278@172.66.44.200:443?encryption=none&security=tls&sni=testu.erfanfamily.ir&fp=chrome&alpn=http%2F1.1&insecure=0&allowInsecure=0&type=ws&host=testu.erfanfamily.ir&path=%2Fvl%2FKIG6R8zHJxjnEPNXIeF7YLPOG9oN4%3Fed%3D2560#4",
         "vless://08d047ca-af68-11f1-ab81-2fc870e97527@104.20.18.167:443?encryption=none&security=tls&sni=yes.docom47457.workers.dev&fp=chrome&alpn=http%2F1.1&type=ws&host=yes.docom47457.workers.dev&path=%2Fdk1.vpnjantit.com%3A10002%2Fvpnjantit#%F0%9F%87%A9%F0%9F%87%B0%20Denmark",
