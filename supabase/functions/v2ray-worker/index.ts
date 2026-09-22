02adebf109bdc70b3e0576038a609049e35cace7
akerfan1 <124583392+akerfan1@users.noreply.github.com>
2026-09-22 13:40:25 +0330

    Update ENABLED_CONFIGS

--- supabase/functions/v2ray-worker/index.ts
+++ supabase/functions/v2ray-worker/index.ts
@@ -47,7 +47,7 @@ function getRandomProxyIP() {
 
 const ENABLED_CONFIGS = {
   bestload: true,   // ⚡ best load (بدون فرگمنت)
-  irancell: true,   // Fragment (Irancell)
+  irancell: false,   // Fragment (Irancell)
   beta: true         // finalMask (Beta)
 };
 
